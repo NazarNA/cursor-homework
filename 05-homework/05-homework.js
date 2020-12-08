ffbd1de2519dcc2421328a9f1f558ad29fb848e5
Nazar <nazarnovosad610@gmail.com>
2020-12-08 09:05:42 +0200

    homework

---
+++ 05-homework/05-homework.js
@@ -0,0 +1,28 @@
+function getRandomArray(length,min,max){
+    let result = [];
+    for(let i = 0; i < length; i++){
+        result.push(Math.round(Math.random() * (max - min) + min))
+    }
+    return result
+}
+
+function getAverage(...numbers){
+    let result = numbers.filter(el => Number.isInteger(el)).map(el => parseInt(el))
+    let sum = result.reduce((acc, el) => acc + el) / result.length
+    return sum
+}
+
+function filterEvenNumbers (...nums){
+    let sorted = nums.filter(el => el % 2 !== 0)
+    return sorted
+}
+
+function countPositiveNumbers(...nums){
+    let sorted = nums.filter(el => el > 0)
+    return sorted.length
+}
+
+function getDividedByFive(...nums){
+    let sorted = nums.filter(el => el % 5 === 0)
+    return sorted
+}
